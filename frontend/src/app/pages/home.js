95ad29f41b30e36251cc2ff789c30bd8b209384e
Antonio Fregoso <antoniofregoso@protonmail.com>
2026-06-30 21:30:28 -0600

    [FIX] Router problems

--- frontend/src/app/pages/home.js
+++ frontend/src/app/pages/home.js
@@ -1,4 +1,5 @@
 export function home(req, router){
+    console.log('**************', router.pathFor("dash"))
     document.getElementById('app').innerHTML = `
     <main class="app-shell app-home">
         <h1>Home</h1>
--- frontend/src/app/pages/login.js
+++ frontend/src/app/pages/login.js
@@ -2,7 +2,7 @@ import { renderLogin } from '../components';
 import { currentLang } from '../store';
 
 export function login(req, router){
-
     const appEl = document.getElementById('app');
     appEl.innerHTML = renderLogin(currentLang.value);
+    router.goTo('dash');
 }
